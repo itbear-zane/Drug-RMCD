aa30a1b6376cdaeb05eafd6f3b619618f8f51820
yangjie <1967287841@qq.com>
2025-04-16 20:52:08 +0800

    Jay [重构训练、验证和测试函数]

--- train_util.py
+++ train_util.py
@@ -2,53 +2,41 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 
-from metric import get_sparsity_loss, get_continuity_loss, computer_pre_rec
+import os
 import numpy as np
-import math
 from tqdm import tqdm
 
-class JS_DIV(nn.Module):
-    def __init__(self):
-        super(JS_DIV, self).__init__()
-        self.kl_div=nn.KLDivLoss(reduction='batchmean',log_target=True)
-    def forward(self,p,q):
-        p_s=F.softmax(p,dim=-1)
-        q_s=F.softmax(q,dim=-1)
-        p_s, q_s = p_s.view(-1, p_s.size(-1)), q_s.view(-1, q_s.size(-1))
-        m = (0.5 * (p_s + q_s)).log()
-        return 0.5 * (self.kl_div(m, p_s.log()) + self.kl_div(m, q_s.log()))
+from sklearn.metrics import  roc_auc_score, average_precision_score, roc_curve, confusion_matrix, \
+    precision_recall_curve, precision_score 
+from metric import get_sparsity_loss, get_continuity_loss, binary_cross_entropy, JS_DIV
+from utils import save_checkpoint, load_latest_checkpoint
 
-def train_decouple_causal2(model, opt_gen, opt_pred, dataset, device, args, writer_epoch):
-    TP = 0
-    TN = 0
-    FN = 0
-    FP = 0
+
+def train_one_epoch(model, opt_gen, opt_pred, dataset, device, epoch, args):
     cls_l = 0
     spar_l = 0
     cont_l = 0
     js=0
     train_sp = []
-    batch_len=len(dataset)
     class_losses = []
     gen_losses = []
-    _, epoch = writer_epoch
-    for (batch, (inputs, masks, labels)) in enumerate(tqdm(dataset, desc=f'Training epoch {epoch}')):
+    for (_, (inputs, masks, labels)) in enumerate(tqdm(dataset, desc=f'Training epoch {epoch}')):
         opt_gen.zero_grad()
         opt_pred.zero_grad()
         inputs, org_masks, labels = [item.to(device) for item in inputs], [item.to(device) for item in masks], labels.type(torch.LongTensor).to(device)
 
         #train classification
-        drug_rationales, drug_masks = model.get_rationale(inputs, org_masks, type='drug')
-        prot_ratioanles, prot_masks = model.get_rationale(inputs, org_masks, type='prot')
+        drug_rationales = model.get_rationale(inputs, org_masks, type='drug')
+        prot_ratioanles = model.get_rationale(inputs, org_masks, type='prot')
 
         drug_sparsity_loss = args.sparsity_lambda * get_sparsity_loss(
-            drug_rationales[:, :, 1], drug_masks, args.sparsity_percentage)
+            drug_rationales[:, :, 1], org_masks[0], args.sparsity_percentage)
         prot_sparsity_loss = args.sparsity_lambda * get_sparsity_loss(
-            prot_ratioanles[:, :, 1], prot_masks, args.sparsity_percentage)
+            prot_ratioanles[:, :, 1], org_masks[1], args.sparsity_percentage)
         sparsity_loss = drug_sparsity_loss + prot_sparsity_loss
 
-        train_sp.append((torch.sum(drug_rationales[:, :, 1]) / torch.sum(drug_masks)).cpu().item() + 
-            (torch.sum(prot_ratioanles[:, :, 1]) / torch.sum(prot_masks)).cpu().item())
+        train_sp.append((torch.sum(drug_rationales[:, :, 1]) / torch.sum(org_masks[0])).cpu().item() + 
+            (torch.sum(prot_ratioanles[:, :, 1]) / torch.sum(org_masks[1])).cpu().item())
 
         drug_continuity_loss = args.continuity_lambda * get_continuity_loss(
             drug_rationales[:, :, 1])
@@ -58,8 +46,9 @@ def train_decouple_causal2(model, opt_gen, opt_pred, dataset, device, args, writ
 
         forward_logit = model.pred_forward_logit(inputs, torch.detach(drug_rationales), torch.detach(prot_ratioanles))
         full_text_logits = model.train_one_step(inputs, org_masks)
-        cls_loss = args.cls_lambda * F.cross_entropy(forward_logit, labels)
-        full_text_cls_loss = args.cls_lambda * F.cross_entropy(full_text_logits, labels)
+
+        cls_loss = args.cls_lambda * binary_cross_entropy(forward_logit, labels)[1]
+        full_text_cls_loss = args.cls_lambda * binary_cross_entropy(full_text_logits, labels)[1]
 
         classification_loss = cls_loss + full_text_cls_loss + sparsity_loss + continuity_loss
         class_losses.append(classification_loss.cpu().detach().numpy())
@@ -76,6 +65,16 @@ def train_decouple_causal2(model, opt_gen, opt_pred, dataset, device, args, writ
         name2=[]
         name3=[]
         name4=[]
+        name5=[]
+        name6=[]
+        for idx, p in model.drug_embedding.named_parameters():
+            if p.requires_grad == True:
+                name5.append(idx)
+                p.requires_grad = False
+        for idx, p in model.prot_embedding.named_parameters():
+            if p.requires_grad == True:
+                name6.append(idx)
+                p.requires_grad = False
         for idx,p in model.drug_encoder.named_parameters():
             if p.requires_grad==True:
                 name1.append(idx)
@@ -93,10 +92,10 @@ def train_decouple_causal2(model, opt_gen, opt_pred, dataset, device, args, writ
                 if p.requires_grad == True:
                     name4.append(idx)
                     p.requires_grad = False
-        print('name1={},name2={},name3={},name4={}'.format(len(name1), len(name2), len(name3), len(name4)))
+        print('freeze name1={},name2={},name3={},name4={},name5={},name6={}'.format(name1, name2, name3, name4, name5, name6))
         
-        drug_rationales, drug_masks = model.get_rationale(inputs, org_masks, type='drug')
-        prot_ratioanles, prot_masks = model.get_rationale(inputs, org_masks, type='prot')
+        drug_rationales = model.get_rationale(inputs, org_masks, type='drug')
+        prot_ratioanles = model.get_rationale(inputs, org_masks, type='prot')
 
         #rationales, masks = model.get_rationale(inputs, org_masks)
 
@@ -106,17 +105,18 @@ def train_decouple_causal2(model, opt_gen, opt_pred, dataset, device, args, writ
             jsd_func = JS_DIV()
             jsd_loss = jsd_func(forward_logit, full_text_logits)
         elif args.div=='kl':
-            jsd_loss=nn.functional.kl_div(F.softmax(forward_logit,dim=-1).log(), F.softmax(full_text_logits,dim=-1), reduction='batchmean')
+            jsd_loss=nn.functional.kl_div(F.softmax(forward_logit,dim=-1).log(), 
+                                          F.softmax(full_text_logits,dim=-1), reduction='batchmean')
 
         drug_sparsity_loss = args.sparsity_lambda * get_sparsity_loss(
-            drug_rationales[:, :, 1], masks[0], args.sparsity_percentage)
+            drug_rationales[:, :, 1], org_masks[0], args.sparsity_percentage)
         prot_sparsity_loss = args.sparsity_lambda * get_sparsity_loss(
-            prot_ratioanles[:, :, 1], masks[1], args.sparsity_percentage)
+            prot_ratioanles[:, :, 1], org_masks[1], args.sparsity_percentage)
         sparsity_loss = drug_sparsity_loss + prot_sparsity_loss
         
         train_sp.append(
-            (torch.sum(drug_rationales[:, :, 1]) / torch.sum(masks[0])).cpu().item() + 
-            (torch.sum(prot_ratioanles[:, :, 1]) / torch.sum(masks[1])).cpu().item())
+            (torch.sum(drug_rationales[:, :, 1]) / torch.sum(org_masks[0])).cpu().item() + 
+            (torch.sum(prot_ratioanles[:, :, 1]) / torch.sum(org_masks[1])).cpu().item())
 
         drug_continuity_loss = args.continuity_lambda * get_continuity_loss(
             drug_rationales[:, :, 1])
@@ -135,7 +135,17 @@ def train_decouple_causal2(model, opt_gen, opt_pred, dataset, device, args, writ
         n2=0
         n3=0
         n4=0
+        n5=0
+        n6=0
         #############recover the parameters#############
+        for idx, p in model.drug_embedding.named_parameters():
+            if idx in name5:
+                p.requires_grad = True
+                n5+=1
+        for idx, p in model.prot_embedding.named_parameters():
+            if idx in name6:
+                p.requires_grad = True
+                n6+=1
         for idx,p in model.drug_encoder.named_parameters():
             if idx in name1:
                 p.requires_grad=True
@@ -153,35 +163,119 @@ def train_decouple_causal2(model, opt_gen, opt_pred, dataset, device, args, writ
                 if idx in name4:
                     p.requires_grad=True
                     n4+=1
-        print('recover name1={},name2={},name3={},name4={}'.format(n1, n2, n3, n4))
+        print('recover n1={},n2={},n3={},n4={},n5={},n6={}'.format(n1, n2, n3, n4, n5, n6))
         
-
-        cls_soft_logits = torch.softmax(forward_logit, dim=-1)
-        _, pred = torch.max(cls_soft_logits, dim=-1)
-        # print(pred, labels)
-        # TP predict 和 label 同时为1
-        TP += ((pred == 1) & (labels == 1)).cpu().sum()
-        # TN predict 和 label 同时为0
-        TN += ((pred == 0) & (labels == 0)).cpu().sum()
-        # FN predict 0 label 1
-        FN += ((pred == 0) & (labels == 1)).cpu().sum()
-        # FP predict 1 label 0
-        FP += ((pred == 1) & (labels == 0)).cpu().sum()
         cls_l += cls_loss.cpu().item()
         spar_l += sparsity_loss.cpu().item()
         cont_l += continuity_loss.cpu().item()
         js+=jsd_loss.cpu().item()
-        # print(TP, TN, FN, FP)
+       
         print(f'avg classification_loss: {np.array(class_losses).mean()}')
         print(f'avg gen_loss: {np.array(gen_losses).mean()}')
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    f1_score = 2 * recall * precision / (recall + precision)
-    accuracy = (TP + TN) / (TP + TN + FP + FN)
-    writer_epoch[0].add_scalar('cls', cls_l, writer_epoch[1])
-    writer_epoch[0].add_scalar('spar_l', spar_l, writer_epoch[1])
-    writer_epoch[0].add_scalar('cont_l', cont_l, writer_epoch[1])
-    writer_epoch[0].add_scalar('js', js, writer_epoch[1])
-    writer_epoch[0].add_scalar('train_sp', np.mean(train_sp), writer_epoch[1])
-    return precision, recall, f1_score, accuracy
+    class_loss_epoch = np.array(class_losses).mean()
+    gen_loss_epoch = np.array(gen_losses).mean()
+    # writer_epoch[0].add_scalar('cls', cls_l, writer_epoch[1])
+    # writer_epoch[0].add_scalar('spar_l', spar_l, writer_epoch[1])
+    # writer_epoch[0].add_scalar('cont_l', cont_l, writer_epoch[1])
+    # writer_epoch[0].add_scalar('js', js, writer_epoch[1])
+    # writer_epoch[0].add_scalar('train_sp', np.mean(train_sp), writer_epoch[1])
+    return class_loss_epoch, gen_loss_epoch
 
+
+def evaluate(model, dataloader, device, mode=None):
+    test_losses = []
+    y_label = []
+    y_pred = []
+    model.eval()
+    with torch.no_grad():
+        for (_, (inputs, masks, labels)) in enumerate(dataloader):
+            # inputs, masks, labels = inputs.to(device), masks.to(device), labels.to(device)
+            inputs, masks, labels = [item.to(device) for item in inputs], [item.to(device) for item in masks], labels.to(device)
+            _, _, logits = model(inputs, masks)
+            # pdb.set_trace()
+            n, loss = binary_cross_entropy(logits, labels)
+            test_losses.append(loss.cpu().detach().numpy())
+
+            y_label = y_label + labels.to("cpu").tolist()
+            y_pred = y_pred + n.to("cpu").tolist()
+
+        test_loss_epoch = np.array(test_losses).mean()
+            
+        auroc = roc_auc_score(y_label, y_pred)
+        auprc = average_precision_score(y_label, y_pred)
+
+        if mode == "test":
+            fpr, tpr, thresholds = roc_curve(y_label, y_pred)
+            prec, recall, _ = precision_recall_curve(y_label, y_pred)
+            precision = tpr / (tpr + fpr)
+            f1 = 2 * precision * tpr / (tpr + precision + 0.00001)
+            thred_optim = thresholds[5:][np.argmax(f1[5:])]
+            y_pred_s = [1 if i else 0 for i in (y_pred >= thred_optim)]
+            cm1 = confusion_matrix(y_label, y_pred_s)
+            accuracy = (cm1[0, 0] + cm1[1, 1]) / sum(sum(cm1))
+            sensitivity = cm1[0, 0] / (cm1[0, 0] + cm1[0, 1])
+            specificity = cm1[1, 1] / (cm1[1, 0] + cm1[1, 1])
+            precision1 = precision_score(y_label, y_pred_s)
+
+            return auroc, auprc, np.max(f1[5:]), sensitivity, specificity, accuracy, test_loss_epoch, thred_optim, precision1
+        else:
+            return auroc, auprc, test_loss_epoch
+        
+
+def train(model, optimizer_gen, optimizer_pred, train_loader, val_loader, logger, args):
+    # Resume training
+    start_epoch = 0
+    if args.resume:
+        if os.path.exists(args.model_save_dir):
+            print(f"Reumse training from {args.model_save_dir}")
+            start_epoch, model, optimizer_gen, optimizer_pred = load_latest_checkpoint(args.model_save_dir, start_epoch, model, optimizer_gen, optimizer_pred)
+        else:    
+            raise ValueError(f"{args.model_save_dir} does not exist!")
+    
+    # Set up early stopping
+    best_auroc = 0
+    epochs_without_improvement = 0
+
+    for epoch in range(start_epoch, args.epochs):
+        # Train
+        model.train()
+        class_loss_epoch, gen_loss_epoch = train_one_epoch(model, optimizer_gen, optimizer_pred, train_loader, args.device, epoch, args)
+        logger.info(f"Epoch {epoch + 1}/{args.epochs} - Class Loss: {class_loss_epoch:.4f}, Gen Loss: {gen_loss_epoch:.4f}")
+
+        # Evaluate
+        auroc, auprc, test_loss_epoch = evaluate(model, val_loader, args.device)
+        logger.info(f"Epoch {epoch + 1}/{args.epochs} - Val AUROC: {auroc:.4f}, Val AUPRC: {auprc:.4f}, Val Loss: {test_loss_epoch:.4f}")
+
+        # Create the checkpoint
+        checkpoint = {
+            'epoch': epoch + 1,
+            'model_state_dict': model.state_dict(),
+            'optimizer_gen_state_dict': optimizer_gen.state_dict(),
+            'optimizer_pred_state_dict': optimizer_pred.state_dict(),
+            'auroc': auroc,
+            'train_loss': class_loss_epoch,
+            'train_gen_loss': gen_loss_epoch,
+            'val_loss': test_loss_epoch,
+            'args': args
+        }
+
+        # Check if the current validation loss is the best
+        is_best = auroc > best_auroc
+        if is_best:
+            best_auroc = auroc
+            checkpoint['auroc'] = best_auroc
+            epochs_without_improvement = 0
+        else:
+            epochs_without_improvement += 1
+
+        # Save the checkpoint
+        save_checkpoint(
+            state=checkpoint,
+            is_best=is_best,
+            checkpoint_dir=args.model_save_dir,
+        )
+
+        # Early stopping
+        if epochs_without_improvement >= args.patience:
+            logger.info(f'Early stopping triggered after {epoch + 1} epochs.')
+            break
